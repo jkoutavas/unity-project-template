fa3443d3282fcba5d1984bea7b9db8cf7006cf10
Jay Koutavas <jay@heynow.com>
2022-03-20 09:20:59 -0400

    Added a Capsule object to the scene and a simple CapsuleMB script for it

---
+++ Project/Assets/Scripts/_project/CapsuleMB.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CapsuleMB : MonoBehaviour
+{
+    private int size = 3;
+
+    void Start()
+    {
+        transform.localScale = new Vector3(size, size, size);
+    }
+
+    void Update()
+    {
+
+    }
+}
